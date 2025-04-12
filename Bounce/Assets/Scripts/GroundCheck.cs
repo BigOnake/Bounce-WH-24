14f0dd29deb5ba85956b0918fbb0aa0656526216
Urmat Urustemov <urustemovurmatk@gmail.com>
2025-04-12 00:13:06 -0500

    Basic fps movement

---
+++ Bounce/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public PlayerMovement playerController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        else if (other.gameObject.CompareTag("Floor"))
+        {
+            playerController.SetGrounded(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        else if (other.gameObject.CompareTag("Floor"))
+        {
+            playerController.SetGrounded(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+
+        else if (other.gameObject.CompareTag("Floor"))
+        {
+            playerController.SetGrounded(true);
+        }
+    }
+}
