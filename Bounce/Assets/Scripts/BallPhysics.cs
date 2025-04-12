72a9bb2d9042c3ea90521def33b3cfacb30da9e5
EatManyRocks <157660382+EatManyRocks@users.noreply.github.com>
2025-04-11 20:58:41 -0500

    Completed Basic Ball w/ Physics (& audio+empty stage)

---
+++ Bounce/Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BallPhysics : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float initialMoveSpeed = 5f;
+    [SerializeField] private float maxMoveSpeed = 100f;
+    [SerializeField, Range(0.0f, 1.0f)] private float percentSpeedGain = 0.1f;
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip hitByPlayerSound;
+    [SerializeField] private AudioClip surfaceBounceSound;
+
+    private Rigidbody rb;
+
+    private Vector3 moveDirection;
+    private float currentMoveSpeed = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //delete after testing
+        moveDirection = Random.onUnitSphere;
+        //moveDirection.y = 0;
+        moveDirection.Normalize();
+
+        currentMoveSpeed = initialMoveSpeed;
+        rb.linearVelocity = moveDirection * currentMoveSpeed;
+        //end of delete
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = rb.linearVelocity.normalized * currentMoveSpeed;
+    }
+
+    public void HitByPlayer(Vector3 playerLookDirection, int amountOfTimes = 1)
+    {
+        //movement
+        moveDirection = playerLookDirection.normalized;
+        IncreaseMoveSpeed(amountOfTimes);
+        rb.linearVelocity = moveDirection * currentMoveSpeed;
+
+        //audio
+        if (hitByPlayerSound)
+            AudioSource.PlayClipAtPoint(hitByPlayerSound, transform.position); //find way to create a function that can be used to spawn an audio and randomize pitch
+    }
+
+    private void IncreaseMoveSpeed(int amount = 0)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            currentMoveSpeed += currentMoveSpeed * percentSpeedGain;
+        }
+
+        currentMoveSpeed = Mathf.Min(currentMoveSpeed, maxMoveSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Surface"))
+        {
+            if (surfaceBounceSound)
+                AudioSource.PlayClipAtPoint(surfaceBounceSound, transform.position);
+        }
+    }
+}
